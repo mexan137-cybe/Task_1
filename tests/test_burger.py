bac0d5549ae96e711a1cc9cd6559898321643e11
Belousov Vyacheslav <Mexan1.3.7@yandex.ru>
2026-09-04 01:15:24 +0300

    add test burger

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import Mock
 from burger import Burger
+from ingredient_types import INGREDIENT_TYPE_SAUCE
 
 class TestBurger:
 
@@ -45,3 +46,45 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient_3)
         burger.move_ingredient(0,2)
         assert burger.ingredients == [mock_ingredient_2, mock_ingredient_3, mock_ingredient_1]
+
+    def test_get_price_calculate(self):
+        mock_buns = Mock()
+        mock_buns.get_price.return_value = 105.5
+        mock_ingredient_1, mock_ingredient_2 = Mock(), Mock()
+        mock_ingredient_1.get_price.return_value = 30.5
+        mock_ingredient_2.get_price.return_value = 8.5
+        burger = Burger()
+        burger.set_buns(mock_buns)
+        burger.add_ingredient(mock_ingredient_1)
+        burger.add_ingredient(mock_ingredient_2)
+        assert burger.get_price() == 250.0
+
+    def test_get_receipt_return_final_price(self):
+        mock_buns = Mock()
+        mock_buns.get_price.return_value = 50.0
+        burger = Burger()
+        burger.set_buns(mock_buns)
+        assert "Price: 100.0" in burger.get_receipt()
+
+    def test_get_receipt_return_buns(self):
+        mock_buns = Mock()
+        mock_buns.get_price.return_value = 50.0
+        mock_buns.get_name.return_value = 'Булка с космической пылью'
+        burger = Burger()
+        burger.set_buns(mock_buns)
+        assert 'Булка с космической пылью' in burger.get_receipt()
+
+    def test_get_receipt_return_ingredient(self):
+        mock_buns = Mock()
+        mock_buns.get_price.return_value = 50
+        mock_buns.get_name.return_value = 'Булка с космической пылью'
+
+        mock_ingredient = Mock()
+        mock_ingredient.get_name.return_value = 'Соус из космонавтов'
+        mock_ingredient.get_price.return_value = 20
+        mock_ingredient.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        burger = Burger()
+        burger.set_buns(mock_buns)
+        burger.add_ingredient(mock_ingredient)
+        assert 'sauce Соус из космонавтов' in burger.get_receipt()
+        
